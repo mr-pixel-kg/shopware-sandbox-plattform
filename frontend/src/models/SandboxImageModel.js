8820ef127dc4c7567edcb36cbc885d933fa0e705
Manuel Kienlein <manuel.kienlein@mr-pixel.de>
2026-03-23 17:53:09 +0100

    delete legacy frontend and backend

--- frontend/src/models/SandboxImageModel.js
+++
@@ -1,9 +0,0 @@
-export class SandboxImageModel {
-  constructor(title, thumbnailUrl, imageName, description, infoLink) {
-    this.title = title;
-    this.thumbnailUrl = thumbnailUrl;
-    this.imageName = imageName;
-    this.description = description;
-    this.infoLink = infoLink;
-  }
-}
--- frontend/src/services/auditLogService.js
+++
@@ -1,9 +0,0 @@
-import ApiService from "@/services/apiService.js";
-
-class AuditLogService {
-  async getAllLogEntries() {
-    return await ApiService.request("get", "/api/auditlog");
-  }
-}
-
-export default new AuditLogService();
