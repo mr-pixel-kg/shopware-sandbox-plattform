9d3e6ac9e2079cd6c0ee8d59f84c500497208d28
Sebastian Stepper <Sebastian.Stepper@mr-pixel.de>
2026-03-24 07:10:35 +0100

    rm: legacy frontend and backend folder

--- frontend/src/models/SandboxEnvironment.js
+++
@@ -1,7 +0,0 @@
-export class SandboxEnvironment {
-  constructor(sandboxId, image, url) {
-    this.sandboxId = sandboxId;
-    this.image = image;
-    this.url = url;
-  }
-}
--- frontend/src/models/SandboxImage.js
+++
@@ -1,7 +0,0 @@
-export class SandboxImage {
-  constructor(title, imageName, thumbnail) {
-    this.title = title;
-    this.imageName = imageName;
-    this.thumbnail = thumbnail;
-  }
-}
