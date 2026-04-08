90a46e2e0418efb25f3fb76066fd9e1ce907196f
Sebastian Stepper <Sebastian.Stepper@mr-pixel.de>
2026-04-08 16:20:58 +0200

    chore: add shadcn-vue combobox

---
+++ web/src/components/ui/combobox/Combobox.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ComboboxRoot, useForwardPropsEmits } from "reka-ui"
+
+import type { ComboboxRootEmits, ComboboxRootProps } from "reka-ui"
+
+const props = defineProps<ComboboxRootProps>()
+const emits = defineEmits<ComboboxRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+  <ComboboxRoot
+    v-slot="slotProps"
+    data-slot="combobox"
+    v-bind="forwarded"
+  >
+    <slot v-bind="slotProps" />
+  </ComboboxRoot>
+</template>
---
+++ web/src/components/ui/combobox/ComboboxViewport.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { reactiveOmit } from "@vueuse/core"
+import { ComboboxViewport, useForwardProps } from "reka-ui"
+
+import { cn } from "@/lib/utils"
+
+import type { ComboboxViewportProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+
+const props = defineProps<ComboboxViewportProps & { class?: HTMLAttributes["class"] }>()
+
+const delegatedProps = reactiveOmit(props, "class")
+
+const forwarded = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <ComboboxViewport
+    data-slot="combobox-viewport"
+    v-bind="forwarded"
+    :class="cn('max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto', props.class)"
+  >
+    <slot />
+  </ComboboxViewport>
+</template>
